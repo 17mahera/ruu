88d17a0ce58ddd00cffb0a4fa49a593fe7363724
17mahera <maomaoam.aki@gmail.com>
2017-05-26 22:14:13 -0400

    Resetting Ruubot
    overhaul from Ruu-Bot

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruu-AndroidOS
+Discord Bot
+The .env contains the Discord Token
+Built following the tutorial by Shayne Hartford in https://www.youtube.com/watch?v=AqqfnhuqVLc
+
+Ruu is Waifu
---
+++ ruu.js
@@ -0,0 +1,61 @@
+// Requires
+const Discord = require('discord.js')
+
+// Create an instance of a Discord client
+const client = new Discord.Client()
+
+// The bot is ready
+client.on('ready', () => {
+  console.log('Bot Started')
+})
+
+
+bot.on('message', message => { // when message is sent
+    if (message.content ===("<>")){ // when message is !bot
+        message.channel.sendMessage("Shinies!"); // send running message into the channel where the message was sent
+    }
+})
+client.on('message', (message) => {
+  // Set the prefix
+  let prefix = config.prefix;
+
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + 'help'))
+  {
+    message.channel.send('I don\'t know what to help with yet!');
+  }
+  else if (message.content.startsWith(prefix + 'idoruu'))
+  {
+    message.channel.send('http://i.imgur.com/Emk9LEb.png!');
+  }
+
+  else if(message.content.startsWith(config.prefix + 'prefix'))
+  {
+    // if it's not me it won't work
+    if(message.author.id !== config.ownerID) return;
+  // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+  let newPrefix = message.content.split(' ').slice(1, 2)[0];
+  // change the configuration in memory
+  config.prefix = newPrefix;
+
+  // Now we have to save the file.
+  fs.writeFile('./config.json', JSON.stringify(config));
+  }
+  else if(message.content.startsWith(config.prefix + 'waifu'))
+  {
+    // if it's not me it won't work
+    if(message.author.id !== config.ownerID)
+    {
+      message.channel.send('Not AA-senpai go away');
+    }
+    else
+    {
+    message.channel.send('AA-senpai!');
+    }
+
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
