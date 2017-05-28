1c7bcf307b0239a62cb5d0cbf1c681e3c2101177
17mahera <maomaoam.aki@gmail.com>
2017-05-27 21:24:05 -0400

    added semicolon

--- aa.js
+++ aa.js
@@ -7,7 +7,7 @@ const client = new Discord.Client()
 
 // The bot is ready
 client.on('ready', () => {
-  console.log('Bot Started')
+  console.log('Bot Started');
   client.guild(config.serverID).channel('235780001268629504').send("AutonomousAnswerer now online!");
 
 })
